a8d03275d1f5a91b042ce4a1bd97300921671fc9
yutotetuota <34832037+yutotetuota@users.noreply.github.com>
2018-06-27 15:48:08 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from distutils.core import setup, Extension
 
 vips_vipstar_module = Extension('vips_vipstar',
-                            sources = ['vipstar.c'],
-                            extra_compile_args=['-march=native', '-funroll-loops', '-fomit-frame-pointer'],
-                            include_dirs=['.'])
+                               sources = ['vipstar.c'],
+                               include_dirs=['.'])
 
 setup (name = 'vips_vipstar',
        version = '1.0',
-       description = 'Bindings for vipstar proof of work used by vipstarcoin',
+       description = 'Bindings for vipstar proof of work used by VIPSTARCOIN',
        ext_modules = [vips_vipstar_module])
